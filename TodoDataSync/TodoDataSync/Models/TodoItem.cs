42327a013d8b95513293dcb7718791e82f4e694b
Satoru Fujimori <satoru.fujimori@gmail.com>
2019-05-21 21:30:20 +0900

    Added replace item when the item is update

--- TodoDataSync/TodoDataSync/Models/TodoItem.cs
+++ TodoDataSync/TodoDataSync/Models/TodoItem.cs
@@ -5,7 +5,7 @@ namespace TodoDataSync.Models
 {
     public class TodoItem
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Notes { get; set; }
         public bool Done { get; set; }
