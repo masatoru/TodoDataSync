977bce0c55ef1392dffd0e8c1f120faea9cfde06
Satoru Fujimori <satoru.fujimori@gmail.com>
2019-06-05 15:45:59 +0900

    Added implementation of push

--- TodoDataSync/TodoDataSync/App.cs
+++ TodoDataSync/TodoDataSync/App.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AppCenter;
+﻿using System.Threading.Tasks;
+using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Auth;
 using Microsoft.AppCenter.Crashes;
@@ -25,8 +26,14 @@ namespace TodoDataSync
             MainPage = nav;
         }
 
-        protected override void OnStart()
+        protected override async void OnStart()
         {
+
+            if (!AppCenter.Configured)
+            {
+                Push.PushNotificationReceived += this.Push_PushNotificationReceived;
+            }
+
             // Setup AppCenter
             AppCenter.Start($"android={AppCenterConfiguration.Android};" +
                             $"ios={AppCenterConfiguration.iOS}",
@@ -35,6 +42,28 @@ namespace TodoDataSync
                 typeof(Analytics),
                 typeof(Crashes),
                 typeof(Push));
+        }
+
+        private async void Push_PushNotificationReceived(object sender, PushNotificationReceivedEventArgs e)
+        {
+            // Add the notification message and title to the message
+            var summary = $"Push notification received:" +
+                          $"\n\tNotification title: {e.Title}" +
+                          $"\n\tMessage: {e.Message}";
+
+            // If there is custom data associated with the notification,
+            // print the entries
+            if (e.CustomData != null)
+            {
+                summary += "\n\tCustom data:\n";
+                foreach (var key in e.CustomData.Keys)
+                {
+                    summary += $"\t\t{key} : {e.CustomData[key]}\n";
+                }
+            }
+
+            // Send the notification summary to debug output
+            await Application.Current.MainPage.DisplayAlert("Message from Push", summary, "ok");
         }
     }
 }
