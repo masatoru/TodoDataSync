05818b502f2cfd6239c2153595a6731da377319b
Satoru Fujimori <satoru.fujimori@gmail.com>
2019-08-04 00:24:16 +0900

    Added AppCenterConfiguration.cs

---
+++ TodoDataSync/TodoDataSync/Models/AppCenterConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoDataSync.Models
+{
+    public static class AppCenterConfiguration
+    {
+        public const string Android = "Your App Cener Key for Android";
+        public const string iOS = "";
+        public const string UWP = "";
+    }
+}
